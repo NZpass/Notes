e5c9c0cb0d0a9de79a2d02a8c47faeecdf3574b6
NZpass <snake_28@mail.ru>
2024-02-10 16:02:18 +0300

    3 commit

--- main.py
+++ main.py
@@ -3,22 +3,66 @@
 # Программа должна уметь создавать заметку, сохранять её, читать список заметок,
 # редактировать заметку, удалять заметку.
 
-from PyQt5.QtWidgets import QApplication, QMainWIndow, QMassageBox
-from PyQt5.QtCore import Qt, QTimer
-import sys
-import datetime
-def aplic():
-    app = QApplication(sys.argv)
-    okno = Window()
-    okno.show()
-    sys.exit(app.exec_())
+import os
 
-if _name_ == '_main_':
-    aplic()
+def create_note():
+    note_name = input("Введите название заметки: ")
+    note_content = input("Введите текст заметки: ")
 
-class Window(QMainWIndow):
-    def _init_(self):
-        super(Window, self)._init_()
+    with open(note_name + ".txt", "w") as file:
+        file.write(note_content)
 
-        self.setWindowTitle("programs")
-        self.setGeometry(400,400,600,300)
+    print("Заметка создана успешно!")
+
+def read_notes():
+    notes = os.listdir()
+    for note in notes:
+        if note.endswith(".txt"):
+            with open(note, "r") as file:
+                print(note[:-4] + ":")
+                print(file.read())
+                print("-----------------------")
+
+def edit_note():
+    note_name = input("Введите название заметки, которую хотите отредактировать: ")
+    if os.path.exists(note_name + ".txt"):
+        with open(note_name + ".txt", "w") as file:
+            note_content = input("Введите новый текст заметки: ")
+            file.write(note_content)
+        print("Заметка отредактирована успешно!")
+    else:
+        print("Заметка не найдена")
+
+def delete_note():
+    note_name = input("Введите название заметки, которую хотите удалить: ")
+    if os.path.exists(note_name + ".txt"):
+        os.remove(note_name + ".txt")
+        print("Заметка удалена успешно!")
+    else:
+        print("Заметка не найдена")
+
+def main():
+    while True:
+        print("1. Создать заметку")
+        print("2. Показать список заметок")
+        print("3. Редактировать заметку")
+        print("4. Удалить заметку")
+        print("5. Выход")
+
+        choice = input("Выберите действие: ")
+
+        if choice == "1":
+            create_note()
+        elif choice == "2":
+            read_notes()
+        elif choice == "3":
+            edit_note()
+        elif choice == "4":
+            delete_note()
+        elif choice == "5":
+            break
+        else:
+            print("Некорректный ввод, попробуйте еще раз.")
+
+if __name__ == "__main__":
+    main()
