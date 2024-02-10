80c1c64397346f77950613f1f31176953880208d
NZpass <snake_28@mail.ru>
2024-02-10 15:35:59 +0300

    2 commit

--- main.py
+++ main.py
@@ -3,3 +3,22 @@
 # Программа должна уметь создавать заметку, сохранять её, читать список заметок,
 # редактировать заметку, удалять заметку.
 
+from PyQt5.QtWidgets import QApplication, QMainWIndow, QMassageBox
+from PyQt5.QtCore import Qt, QTimer
+import sys
+import datetime
+def aplic():
+    app = QApplication(sys.argv)
+    okno = Window()
+    okno.show()
+    sys.exit(app.exec_())
+
+if _name_ == '_main_':
+    aplic()
+
+class Window(QMainWIndow):
+    def _init_(self):
+        super(Window, self)._init_()
+
+        self.setWindowTitle("programs")
+        self.setGeometry(400,400,600,300)
